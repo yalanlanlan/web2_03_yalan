2b9dce88e55e6343f01e641eacb44faffc00e1f8
lan <leaps7bounds@gmail.com>
2019-02-10 17:03:25 -0500

    add

---
+++ start/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	
+$.ajax({
+	type: 'GET',
+	url: 'data.json',
+	dataType: 'json',
+	beforeSend: function(xhr){
+	    if (xhr.overrideMimeType) {
+	      xhr.overrideMimeType("application/json");
+	    }
+  	},
+	success: function(data) {
+		
+		let placeArray = data.places;
+    let places="<ul>";
+    $.each(placeArray, function(key, value){
+		places+='<li><a href="#" data-year="'+value.year+'">' + value.place + '<div class="inner" id ></div></a></li>';
+    });
+    //each--smplify for loop^
+    places += "<ul>";
+    console.log(places);
+    $('#content').append(places);
+    //append--attach
+	},
+	error: function() {
+		window.alert('Failed');
+	}
+});
+
+});
+$(document).ajaxSuccess(function(){
+	
+    $("li a").click(function(){
+
+    $('.inner').slideUp();
+	let year = $(this).data('year');
+	$('.inner').html(year);
+
+    $(this).find('.inner').slideDown();
+    
+    })
+
+});
+
+// $(document).ready(function () {
+// 	  var jsonURL = "data.json";
+// 	  $.getJSON(jsonURL, function (json)
+// 	  {
+// 		var imgList= "";
+// 		$.each(json.products, function () {
+// 		  imgList += '<li><img src= "' + this.imgPath + '"></li>';
+// 		});
+		
+// 		$('#dvProdList').append(imgList);
+		
+// 	  });
+// 	});
+	
