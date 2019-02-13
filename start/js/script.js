3a1d9b3c34148d9177846dc5c3c2b8c70347c7db
Lauren Gibbons <laurengibbons@lgibbons.local>
2019-02-12 21:05:45 -0500

    Fix images div

--- start/js/script.js
+++ start/js/script.js
@@ -12,22 +12,34 @@ $.ajax({
 	success: function(data) {
 		
 		let placeArray = data.places;
-    let places="<ul>";
-    $.each(placeArray, function(key, value){
-		places+='<li><a href="#" data-year="'+value.year+'">' + value.place + '<div class="inner" id ></div></a></li>';
-    });
-    //each--smplify for loop^
-    places += "<ul>";
-    console.log(places);
-    $('#content').append(places);
-    //append--attach
+		let places="<ul>";
+		$.each(placeArray, function(key, value){
+			places+='<li><a href="#" data-year="'+value.year+'">' + value.place + '<div class="inner" id ></div></a></li>';
+    	});
+    	
+    	//each--smplify for loop^
+    	places += "<ul>";
+    	console.log(places);
+    	$('#content').append(places);
+    	//append--attach
 	},
 	error: function() {
 		window.alert('Failed');
 	}
 });
 
+var jsonURL = "data.json";
+$.getJSON(jsonURL, function(json) {
+	var imgList= "";
+	$.each(json.products, function() {
+		imgList += '<li><img src= "' + this.imgPath + '"></li>';
+	});
+	$('#dvProdList').append(imgList);
 });
+
+});
+
+
 $(document).ajaxSuccess(function(){
 	
     $("li a").click(function(){
@@ -41,18 +53,3 @@ $(document).ajaxSuccess(function(){
     })
 
 });
-
-// $(document).ready(function () {
-// 	  var jsonURL = "data.json";
-// 	  $.getJSON(jsonURL, function (json)
-// 	  {
-// 		var imgList= "";
-// 		$.each(json.products, function () {
-// 		  imgList += '<li><img src= "' + this.imgPath + '"></li>';
-// 		});
-		
-// 		$('#dvProdList').append(imgList);
-		
-// 	  });
-// 	});
-	
