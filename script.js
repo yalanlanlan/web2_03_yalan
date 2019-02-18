28077e020eb78d1a4d51773ed05ec3ea7852f8ca
lan <leaps7bounds@gmail.com>
2019-02-17 21:38:13 -0500

    ok

---
+++ script.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+	
+$.ajax({
+	type: 'GET',
+	url: 'data.json',
+	dataType: 'json',
+	beforeSend: function(xhr){
+	    if (xhr.overrideMimeType) {
+	      xhr.overrideMimeType("application/json");
+	    }
+  	},
+	success: function(data) {
+		
+		let placeArray = data.places;
+		let imgArray = data.imgPath;
+	let places="<ul>";
+	let images="<ul>";
+    $.each(placeArray, function(key, value){
+		places+='<li><a href="#" data-year="'+value.year+'" data-imgPath="'+value.imgPath+'">' + value.place + '<div class="inner"></div></a></li>';
+	});
+	
+    //each--smplify for loop^
+    places += "<ul>";
+    console.log(places);
+    $('#content').append(places);
+    //append--attach
+	},
+	error: function() {
+		window.alert('Failed');
+	}
+});
+});
+var jsonURL = "data.json";
+$.getJSON(jsonURL, function(json) {
+	var imgList= "";
+	$.each(json.products, function() {
+		imgList += '<li><img src= "' + this.imgPath + '"></li>';
+	});
+	$('#dvProdList').append(imgList);
+});
+// $(document).ajaxSuccess(function(){
+	
+//     $("li a").click(function(){
+
+//     $('.inner').slideUp();
+// 	let year = $(this).data('year');
+// 	$('.inner').html(year);
+
+//     $(this).find('.inner').slideDown();
+    
+//     })
+
+// });
+var jsonURL = "data.json";
+$.getJSON(jsonURL, function(json) {
+	var imgList= "";
+	$.each(json.places, function() {
+		imgList += '<li><img src= "' + this.imgPath + '"></li>';
+	});
+	console.log(imgList);
+	$('.inner').append(imgList);
+});
+
+
+
+$(document).ajaxSuccess(function(){
+	
+    $("li a").click(function(){
+
+    $('.inner').slideUp();
+	let year = $(this).data('year');
+	
+	let image = $(this).data('imgPath');
+	$('.inner').html(year);
+	$('.inner').html(image);
+
+    $(this).find('.inner').slideDown();
+    $(this).find('.inner').slideDown();
+    
+	})
+});
