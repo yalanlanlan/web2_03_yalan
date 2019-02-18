3556af6b321224f9d643c684301103a32d7a1156
lan <leaps7bounds@gmail.com>
2019-02-17 21:58:48 -0500

    debug

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ $.ajax({
 	success: function(data) {
 		
 		let placeArray = data.places;
-<<<<<<< HEAD
 		let imgArray = data.imgPath;
 	let places="<ul>";
 	let images="<ul>";
@@ -25,33 +24,13 @@ $.ajax({
     console.log(places);
     $('#content').append(places);
     //append--attach
-=======
-		let places="<ul>";
-		$.each(placeArray, function(key, value){
-			places+='<li><a href="#" data-year="'+value.year+'">' + value.place + '<div class="inner" id ></div></a></li>';
-    	});
-    	
-    	//each--smplify for loop^
-    	places += "<ul>";
-    	console.log(places);
-    	$('#content').append(places);
-    	//append--attach
->>>>>>> 3a1d9b3c34148d9177846dc5c3c2b8c70347c7db
 	},
 	error: function() {
 		window.alert('Failed');
 	}
 });
-
-var jsonURL = "data.json";
-$.getJSON(jsonURL, function(json) {
-	var imgList= "";
-	$.each(json.products, function() {
-		imgList += '<li><img src= "' + this.imgPath + '"></li>';
-	});
-	$('#dvProdList').append(imgList);
 });
-<<<<<<< HEAD
+
 // $(document).ajaxSuccess(function(){
 	
 //     $("li a").click(function(){
@@ -77,12 +56,6 @@ $.getJSON(jsonURL, function(json) {
 
 
 
-=======
-
-});
-
-
->>>>>>> 3a1d9b3c34148d9177846dc5c3c2b8c70347c7db
 $(document).ajaxSuccess(function(){
 	
     $("li a").click(function(){
@@ -97,10 +70,5 @@ $(document).ajaxSuccess(function(){
     $(this).find('.inner').slideDown();
     $(this).find('.inner').slideDown();
     
-<<<<<<< HEAD
 	})
-=======
-    })
-
->>>>>>> 3a1d9b3c34148d9177846dc5c3c2b8c70347c7db
 });
