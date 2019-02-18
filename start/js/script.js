799bdd6e95170320cba70c386f59bfe7908bb40e
lan <leaps7bounds@gmail.com>
2019-02-17 21:21:24 -0500

    sass

--- start/js/script.js
+++ start/js/script.js
@@ -12,10 +12,13 @@ $.ajax({
 	success: function(data) {
 		
 		let placeArray = data.places;
-    let places="<ul>";
+		let imgArray = data.imgPath;
+	let places="<ul>";
+	let images="<ul>";
     $.each(placeArray, function(key, value){
-		places+='<li><a href="#" data-year="'+value.year+'">' + value.place + '<div class="inner" id ></div></a></li>';
-    });
+		places+='<li><a href="#" data-year="'+value.year+'" data-imgPath="'+value.imgPath+'">' + value.place + '<div class="inner"></div></a></li>';
+	});
+	
     //each--smplify for loop^
     places += "<ul>";
     console.log(places);
@@ -28,31 +31,44 @@ $.ajax({
 });
 
 });
+// $(document).ajaxSuccess(function(){
+	
+//     $("li a").click(function(){
+
+//     $('.inner').slideUp();
+// 	let year = $(this).data('year');
+// 	$('.inner').html(year);
+
+//     $(this).find('.inner').slideDown();
+    
+//     })
+
+// });
+var jsonURL = "data.json";
+$.getJSON(jsonURL, function(json) {
+	var imgList= "";
+	$.each(json.places, function() {
+		imgList += '<li><img src= "' + this.imgPath + '"></li>';
+	});
+	console.log(imgList);
+	$('.inner').append(imgList);
+});
+
+
+
 $(document).ajaxSuccess(function(){
 	
     $("li a").click(function(){
 
     $('.inner').slideUp();
 	let year = $(this).data('year');
+	
+	let image = $(this).data('imgPath');
 	$('.inner').html(year);
+	$('.inner').html(image);
 
     $(this).find('.inner').slideDown();
+    $(this).find('.inner').slideDown();
     
-    })
-
+	})
 });
-
-// $(document).ready(function () {
-// 	  var jsonURL = "data.json";
-// 	  $.getJSON(jsonURL, function (json)
-// 	  {
-// 		var imgList= "";
-// 		$.each(json.products, function () {
-// 		  imgList += '<li><img src= "' + this.imgPath + '"></li>';
-// 		});
-		
-// 		$('#dvProdList').append(imgList);
-		
-// 	  });
-// 	});
-	
